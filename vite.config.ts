d4977043526eb3142155147fbc56bfe5e8443b22
skoonapp <tom334957@gmail.com>
2025-09-05 20:00:14 +0530

    feat: Update PWA icon paths
    Renames PWA icon assets and updates their references in `index.html` and `vite.config.ts` to use the new naming convention.

--- vite.config.ts
+++ vite.config.ts
@@ -26,19 +26,19 @@ export default defineConfig({
         "orientation": "portrait-primary",
         "icons": [
           {
-            "src": "icon-192.png",
+            "src": "pwa-icon-192.png",
             "sizes": "192x192",
             "type": "image/png",
             "purpose": "any"
           },
           {
-            "src": "icon-512.png",
+            "src": "pwa-icon-512.png",
             "sizes": "512x512",
             "type": "image/png",
             "purpose": "any"
           },
           {
-            "src": "icon-maskable-512.png",
+            "src": "pwa-maskable-icon-512.png",
             "sizes": "512x512",
             "type": "image/png",
             "purpose": "maskable"
